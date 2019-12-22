0e68a30fefcbf44bd3f07e55d1b61efe7985d789
Ahsan Fayyaz <ahsan@iknox.com>
2019-12-22 16:56:39 -0500

    adding photos and fixing broken files

--- app/src/main/java/com/example/landmarksquizapp/MainActivity.java
+++ app/src/main/java/com/example/landmarksquizapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.landmarksquizapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonBegin = findViewById(R.id.button_begin);
+        buttonBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beginQuiz();
+
+            }
+        });
+    }
+
+
+    private void beginQuiz() {
+        Intent intent = new android.content.Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
