40debd4291805f71a00b0b3aab6982a74a5cefa2
Ahsan Fayyaz <ahsan@iknox.com>
2019-12-22 15:45:56 -0500

    more changes

--- app/src/main/java/com/example/landmarksquizapp/MainActivity.java
+++ app/src/main/java/com/example/landmarksquizapp/MainActivity.java
@@ -1,15 +1,8 @@
-package com.example.landmarksquiz;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+package com.example.landmarksquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.landmarksquizapp.QuizActivity;
-import com.example.landmarksquizapp.R;
-
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,20 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button buttonBegin = findViewById(R.id.button_begin);
-        buttonBegin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                beginQuiz();
-
-            }
-        });
-    }
-
-
-    private void beginQuiz() {
-        Intent intent = new android.content.Intent(MainActivity.this, QuizActivity.class);
-        startActivity(intent);
     }
 }
